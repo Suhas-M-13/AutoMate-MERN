8e741a38764302d7e6cbe3c5f7b976cd0e345894
GaneshSPandith <ganeshspandith@gmail.com>
2025-01-31 14:37:09 +0530

    Updated Index.js

--- Auth/backend/index.js
+++ Auth/backend/index.js
@@ -16,24 +16,29 @@ const PORT = process.env.PORT || 5000
 
 const __dirname = path.resolve()
 
-app.use(cors())
-
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth",authRoutes)
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"/frontend/dist")))
-
+    
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
     })
+    app.use(cors())
+}
+else{
+    app.use(cors({
+        origin : "http://localhost:5173",
+        credentials : true,
+    }))
 }
 
 app.get("/",(req,res)=>{
     res.send("Hello World!!")
 })
+app.use("/api/auth",authRoutes)
 
 
 app.listen(PORT,()=>{
