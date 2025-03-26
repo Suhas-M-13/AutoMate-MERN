a92c5b6bf3fb71052985b0428fff6999a24d7216
M Suhas <suhasmkashyap@gmail.com>
2025-03-26 22:59:52 +0530

    Created seperate folders for UI

--- Auth/backend/db/connection.js
+++ Auth/backend/db/connection.js
@@ -1,20 +1,15 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 
-
-export const connectDB = async() => {
+export const connectDB = async () => {
     try {
         console.log(process.env.MONGO_URI);
-        
+
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDb connected ${conn.connection.host}`);
-        
+
     } catch (error) {
-        console.log("Error in connecting Mongodb : "+error.message);
+        console.log("Error in connecting Mongodb : " + error.message);
         process.exit(1)
     }
 }
-
-// export{ 
-//     connectDB
-// }
